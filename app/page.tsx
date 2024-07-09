3c2d53589fc87853e1acc0973ef5a04d41a85704
Arush Mehrotra <43364437+arush-mehrotra@users.noreply.github.com>
2024-07-08 22:21:50 -0400

    updated profile pic

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import profilePic from "../public/profile.jpg";
+import profilePic from "../public/profile.jpeg";
 
 const navigation = [
 	{ name: "Projects", href: "/projects" },
