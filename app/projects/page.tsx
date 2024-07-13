6d2637fb22e14861f4034ed8fcbab18adb0c475c
Arush Mehrotra <43364437+arush-mehrotra@users.noreply.github.com>
2024-07-13 12:44:57 -0400

    added projects and updated resume

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -4,6 +4,8 @@ import Image from 'next/image';
 import FIFA from '../../images/fifa.jpeg';
 import AIRBNB from '../../images/airbnb.png';
 import LINUX from '../../images/linux.png';
+import PFJ from '../../images/pfj.png';
+import CC from '../../images/CC.png';
 
 export default function Projects() {
 
@@ -107,6 +109,32 @@ export default function Projects() {
 
         <div style={styles.project}>
           <div style={styles.projectInfo}>
+            <h2 style={styles.projectTitle}>Partners for Justice</h2>
+            <p style={styles.projectDate}>Feb - May 2024</p>
+            <p style={styles.projectDescription}>
+            In collaboration with Partners for Justice, a national nonprofit organization dedicated to transforming the legal system, I developed a tool that automates data analysis for advocate databases stored in Airtable. This tool addresses the challenges of manual data analysis and repetitive tasks across multiple databases as the number of advocate offices grows.
+
+Using the Airtable API, the tool pulls data from specified databases and performs queries to calculate key insights, such as the number of cases opened and the comparison between entered and provided service goals. Users can specify date ranges for targeted analysis, reducing a process that once took days to just minutes. This tool significantly enhances efficiency and accuracy in data reporting and analysis, supporting Partners for Justice's mission to save clients from incarceration and connect them with vital social services.
+            </p>
+            <div style={styles.projectTags}>
+              <span style={styles.tag}>Pandas</span>
+              <span style={styles.tag}>Airtable</span>
+              <span style={styles.tag}>Python</span>
+            </div>
+          </div>
+          {isLargeScreen && (
+          <Image
+            src={PFJ}
+            alt="Project Image"
+            width={300}
+            height={150}
+            className="rounded-lg"
+          />
+        )}
+        </div>
+
+        <div style={styles.project}>
+          <div style={styles.projectInfo}>
             <h2 style={styles.projectTitle}>AirBnB Price Prediction Machine Learning Model</h2>
             <p style={styles.projectDate}>April - May 2023</p>
             <p style={styles.projectDescription}>
@@ -153,7 +181,34 @@ export default function Projects() {
           />
         )}
         </div>
-        {/* Add more projects here */}
+        
+        <div style={styles.project}>
+          <div style={styles.projectInfo}>
+            <h2 style={styles.projectTitle}>CommonCents Web Application</h2>
+            <p style={styles.projectDate}>Sep - Dec 2021</p>
+            <p style={styles.projectDescription}>
+            In collaboration with Penn Spark, we developed CommonCents, a gamified edtech platform designed to empower students with the knowledge to manage their money and achieve financial independence. This platform aims to make personal finance learning innovative, interesting, and inclusive for a diverse audience, ranging from high school students to undergraduates.
+
+As a team of four developers, we drove the application from development to deployment. CommonCents now attracts over 2000 monthly visits across 8 campuses and serves 1600 students.
+            </p>
+            <div style={styles.projectTags}>
+              <span style={styles.tag}>ReactJS</span>
+              <span style={styles.tag}>Node</span>
+              <span style={styles.tag}>Express</span>
+              <span style={styles.tag}>MongoDB</span>
+            </div>
+          </div>
+          {isLargeScreen && (
+          <Image
+            src={CC}
+            alt="Project Image"
+            width={300}
+            height={150}
+            className="rounded-lg"
+          />
+        )}
+        </div>
+
       </div>
     );
   }
