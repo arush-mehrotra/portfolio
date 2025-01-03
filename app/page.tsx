e873ab57cdedb31f3b3bcecb6ccb50c13884573a
Arush Mehrotra <43364437+arush-mehrotra@users.noreply.github.com>
2025-01-02 18:34:05 -0800

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -71,10 +71,10 @@ const workExperience = [
 	},
 	{
 		company: "Penn Engineering",
-		position: "Teaching Assistant | CIS 5450 (Big Data Analytics) | CIS 1200 (Programming Languages and Techniques)",
+		position: "Teaching Assistant",
 		type: "Part-Time",
 		date: "Sep 2022 - Present",
-		description: "Lead weekly recitations, grade assignments, and hold office hours for 50+ students. Create and grade exams, quizzes, and projects. Mentor students in Python, Java, and SQL.",
+		description: "TA for CIS 5450 (Big Data Analytics) and CIS 1200 (Programming Languages & Techniques). Lead weekly recitations, grade assignments, and hold office hours for 50+ students. Create and grade exams, quizzes, and projects. Mentor students in Python, Java, and SQL.",
 	}
   ];  
 
